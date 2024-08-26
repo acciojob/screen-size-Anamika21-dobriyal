3e3d878f035ed77928c197cd3eb6ace9d83b9c53
Anamika <anamikadobriyal21@gmail.com>
2024-08-26 20:01:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function updateSizeInfo() {
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+            document.getElementById('sizeInfo').innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
+        }
+
+        // Call the function initially to set the size on page load
+        updateSizeInfo();
+
+        // Add event listener to update the size whenever the window is resized
+        window.addEventListener('resize', updateSizeInfo);
